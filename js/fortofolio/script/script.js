778b79909398d7e024852cd1c41c5cc0b12e78a4
yopi-develove9 <yoyokertadipura@gmai.com>
2023-06-11 15:57:43 +0700

    first commit

---
+++ js/fortofolio/script/script.js
@@ -0,0 +1,64 @@
+//operator pada javascript adalah sebuah simbol yang digunakan untk melakukan operasi.
+// 1. operator dalam javascript
+// a. aritmatika/binary
+// b. penugasan/binary
+// c. perbandingan/binary
+// d. logika/binary
+// e. string/binary
+// f. kondisional/ternary
+// g. typof/unary
+// operator binary membutuhkan 2 operand. operand1 operator operand2 contoh 1 + 1 =2
+// operator ternary adalah operator yang membutuhkan 3 operator
+
+
+ // operator logika
+ // &&
+ // ||
+ // !
+  // operator +
+  // jika bertemu angka menjadi operasi aritmatika
+  // jika bertemu huruf menjadi penggabung string
+
+  // operator typof
+  // typof(operand)
+
+
+
+
+  // String/ tipe data string dalam javascript digunakan untuk merepesentasikan data tekstual./ plain text. string pembungkus adalah tanda kutip dua "" atau kutip satu''.
+  // penggunaannya bebas asal berpasangan.
+  // Boolean adalah sebuah tipe data untuk merepesentasikan untuk logika true atau false.
+  // Variable adalah sebuah tempat/wadah yang memiliki nama, yang digunakan untuk menyimpan nilai.
+  // memahami variable harus mengerti tentang. deklarasi, inisialisai, assigment.
+
+
+  // DEKLARASI var, let, const
+   // var x; // deklarasi dan inisialisasi
+   // x = 20; //assigment
+   // deklarasi variable adalah var <nama variable>
+   // var gak boleh pakai spasi
+   // var gak boleh mengandung angka di depan huruf
+   //  var boleh menggunakan angka di belakang atau tengah
+   // var boleh memakai tanda_ atau dolar atau kombinasi huruf besar atau camel case.
+   // keyword & reserver word kata yang tidak boleh digunakan.
+   //  shorthand/
+     // var nama;
+     // var umur; 
+     // var lulus;
+
+
+     // nama = "yopi";
+     // umur = 31;
+     // lulus = true;
+
+     // shorthand/ kita bisa menulis satu var saja conto:
+     // var nama = "yopi",
+     // umur = 31,
+     // lulus = true;
+
+
+
+
+     console.log('helloworld');
+     var x = 10;
+     console.log('isi dari variable x adalah ' + x);
